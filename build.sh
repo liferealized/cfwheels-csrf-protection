44b43c958607d3db3e352ce24d19b33034adce78
Chris Peters <chris@minimalorange.com>
2016-09-01 23:27:15 -0400

    Update build script for 0.1.0

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-CSRF_PROTECTION_VERSION="0.0.5"
+CSRF_PROTECTION_VERSION="0.1.0"
 CSRF_PROTECTION_ZIP="CsrfProtection-$CSRF_PROTECTION_VERSION.zip"
-zip $CSRF_PROTECTION_ZIP CsrfProtection.cfc LICENSE README.md controller/* view/*
+zip $CSRF_PROTECTION_ZIP CsrfProtection.cfc LICENSE README.md controller/* events/* view/*
